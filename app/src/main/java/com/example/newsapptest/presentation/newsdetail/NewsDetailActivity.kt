62efd7a4dd7179a42af8994f13d9893311c99e4f
arif-summitglobal <arief21.hidayat21@gmail.com>
2025-02-26 00:03:45 +0700

    Update NewsDetailActivity.kt

--- app/src/main/java/com/example/newsapptest/presentation/newsdetail/NewsDetailActivity.kt
+++ app/src/main/java/com/example/newsapptest/presentation/newsdetail/NewsDetailActivity.kt
@@ -11,7 +11,6 @@ import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.bitmap.RoundedCorners
 import com.bumptech.glide.request.RequestOptions
 import com.example.newsapptest.R
-import com.example.newsapptest.data.mapper.toEntity
 import com.example.newsapptest.databinding.ActivityNewsDetailBinding
 import com.example.newsapptest.domain.entity.Article
 import com.example.newsapptest.domain.entity.ArticleLocal
